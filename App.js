f232202f1789d83d9cbb4653d219dde588783f68
Ben Straatsma <benstraatsma@gmail.com>
2018-05-02 15:01:51 -0400

    Added logo, some buttons, and some textboxes

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View, Image, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'space-between',
+   alignItems: 'center',
+   margin: 20,
+   padding: 10
+  }
+})
+
+export default class Bananas extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullNameText: '', 
+      usernameText: '', 
+      passwordText: '', 
+      retypePasswordText: '', 
+      status: true
+    };
+  }
+
+  signUpClick() {
+    Alert.alert('Signing up!')
+  }
+
+  ShowHideTextComponentView = () =>{
+    if(this.state.status) {
+      this.setState({status: false})
+    }
+    else {
+      this.setState({status: true})
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to Friend Request</Text>
+        <Image source={require('./assets/logo.png')} style={{width: 200, height: 200}} />
+        <TextInput style={{height: 40, width: 200}} placeholder= 'Full Name'/>
+        <TextInput
+          style={{height: 40, width: 200}}
+          placeholder="Username"
+        />
+        <TextInput
+          style={{height: 40, width: 200}}
+          placeholder="Password"
+        />
+        <TextInput
+          style={{height: 40, width: 200}}
+          placeholder="Retype Password"
+        />
+        <Button
+          onPress={this.signUp}
+          title="Sign Up"
+          color="#ffb028"
+        />
+        <Button
+          onPress={this.ShowHideTextComponentView}
+          title="I Already Have an Account"
+          color="#ffb028"
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

a6bfe7119e91de99a583bde927673c35a62d9a18
Ben Straatsma <benstraatsma@gmail.com>
2018-05-03 15:25:06 -0400

    Changed class name

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const styles = StyleSheet.create({
   }
 })
 
-export default class Bananas extends Component {
+export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {

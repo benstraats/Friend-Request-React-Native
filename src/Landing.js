6132b420500351a462cb2cffa8a2023b93172974
Ben Straatsma <benstraatsma@gmail.com>
2018-05-11 16:11:43 -0400

    Added in ability to see requests for current user

--- src/Landing.js
+++ src/Landing.js
@@ -42,8 +42,10 @@ class Landing extends Component {
       usernameText: this.props.navigation.state.params.username, 
       accessToken: this.props.navigation.state.params.accessToken,
       dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      listDataSource: []
     };
     this.getFriends()
+    this.getRequests()
   }
 
   showUserInfo = () =>{
@@ -83,6 +85,7 @@ class Landing extends Component {
           if (obj._id == friendID) {
             friendInfo.push(obj.name)
             friendInfo.push(obj.email)
+            friendInfo.push("friend")
           }
         })
 
@@ -90,7 +93,56 @@ class Landing extends Component {
       });
 
       self.setState({
-        dataSource: this.state.dataSource.cloneWithRows(friends)
+        listDataSource: this.state.listDataSource.concat(friends)
+      })
+
+      self.setState({
+        dataSource: this.state.dataSource.cloneWithRows(this.state.listDataSource)
+      })
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  getRequests = () =>{
+    let self = this;
+
+    fetch('http://192.168.2.25:3030/myrequests?$limit=50', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + self.state.accessToken
+      }
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      let friends = [];
+
+      responseJson.requests.data.forEach(function(obj) { 
+        let requesterID = obj.requester
+
+        friendInfo = []
+        friendInfo.push(requesterID)
+
+        responseJson.users.data.forEach(function(obj) {
+          if (obj._id == requesterID) {
+            friendInfo.push("Request From: " + obj.name)
+            friendInfo.push(obj.email)
+            friendInfo.push("request")
+          }
+        })
+
+        friends.push(friendInfo)
+      });
+
+      self.setState({
+        listDataSource: this.state.listDataSource.concat(friends)
+      })
+
+      self.setState({
+        dataSource: this.state.dataSource.cloneWithRows(this.state.listDataSource)
       })
     })
     .catch((error) => {
@@ -111,39 +163,45 @@ class Landing extends Component {
   }
 
   onPressFn = (rowData) =>{
-    let self = this;
 
-    fetch('http://192.168.2.25:3030/profile?userID=' + rowData[0], {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + self.state.accessToken
-      }
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      let profile = ''
+    if (rowData[3] === "friend") {
+      let self = this;
 
-      let x = responseJson.data;
-      let y = x[0]
+      fetch('http://192.168.2.25:3030/profile?userID=' + rowData[0], {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + self.state.accessToken
+        }
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let profile = ''
 
-      if (y !== undefined) {
-        let z = y.profile
+        let x = responseJson.data;
+        let y = x[0]
 
-        z.forEach(function(obj) { 
-          profile += obj.key + ": " + obj.value + "\n"
-        });
+        if (y !== undefined) {
+          let z = y.profile
 
-        Alert.alert(rowData[1], profile)
-      }
-      else {
-        Alert.alert(rowData[1], "User has no profile")
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+          z.forEach(function(obj) { 
+            profile += obj.key + ": " + obj.value + "\n"
+          });
+
+          Alert.alert(rowData[1], profile)
+        }
+        else {
+          Alert.alert(rowData[1], "User has no profile")
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+    else {
+      Alert.alert("Accept " + rowData[2] + "?", "Are you sure you want to add " + rowData[2] + "?")
+    }
   }
 
   render() {

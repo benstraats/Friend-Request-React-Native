a15f922675c33cefe301b38652bc706393dc3507
Ben Straatsma <benstraatsma@gmail.com>
2018-08-26 17:58:01 -0400

    Added logout button and stubs for back and logout press

--- src/TopBar.js
+++ src/TopBar.js
@@ -4,30 +4,51 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import {COLORS, STRINGS} from './utils/ProjectConstants'
 
 export default class StatusBarOffset extends Component{
+
+  backPressed = () => {
+    //unsure if this works
+    this.props.navigation.goBack(null)
+  }
+
+  logOutPressed = () => {
+
+  }
+
   render(){
     return(
         <View style={styles.statusBarBackground}>
-          {this.props.mainText !== STRINGS.HOME ? <View style={styles.textViewStyle}>
-            <Ionicons
-              name={'ios-arrow-back'}
-              size={24}
-              style={styles.iconStyle}
-            />
-            <Text style={styles.textStyle}>
-              {this.props.mainText}
-            </Text>
-            <Text style={styles.offsetStyle}>
-              
-            </Text>
-          </View> :
-          <View style={styles.soloText}>
-            <Text style={styles.textStyle}>{this.props.mainText}</Text>
-            </View>}
-            <View
-              style={{
-                borderBottomColor: 'black',
-                borderBottomWidth: 1,
-              }}
+          {this.props.mainText !== STRINGS.HOME ? 
+            <View style={styles.textViewStyle}>
+              <Ionicons
+                name={'ios-arrow-back'}
+                size={24}
+                style={styles.backIconStyle}
+                onIconClicked={() => this.backPressed()}
+                onPress={() => this.backPressed()}
+                />
+              <Text style={styles.textStyle}>
+                {this.props.mainText}
+              </Text>
+              {this.props.mainText !== STRINGS.PROFILE ? 
+                <Text style={styles.offsetStyle}></Text> : 
+                <Ionicons
+                  name={'ios-log-out'}
+                  size={24}
+                  style={styles.logoutIconStyle}
+                  onIconClicked={() => this.logOutPressed()}
+                  onPress={() => this.logOutPressed()}
+                  />
+              }
+            </View> :
+            <View style={styles.soloText}>
+              <Text style={styles.textStyle}>{this.props.mainText}</Text>
+            </View>
+          }
+          <View
+            style={{
+              borderBottomColor: 'black',
+              borderBottomWidth: 1,
+            }}
             />
         </View>
     );
@@ -50,8 +71,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     alignItems: 'center',
   },
-  iconStyle: {
+  backIconStyle: {
     paddingLeft:10,
+  },
+  logoutIconStyle: {
+    paddingRight: 10,
   },
   offsetStyle: {
     paddingRight:20

5e8d50e631fa449d595c0e98a9759093f76257b0
Ben Straatsma <benstraatsma@gmail.com>
2018-08-26 11:20:23 -0400

    added back button to the top bar for search and profile pages

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {COLORS, STRINGS} from './utils/ProjectConstants'
 
 export default class StatusBarOffset extends Component{
   render(){
     return(
         <View style={styles.statusBarBackground}>
-          <View style={styles.textViewStyle}>
+          {this.props.mainText !== STRINGS.HOME ? <View style={styles.textViewStyle}>
+            <Ionicons
+              name={'ios-arrow-back'}
+              size={24}
+              style={styles.iconStyle}
+            />
             <Text style={styles.textStyle}>
               {this.props.mainText}
             </Text>
-          </View>
+            <Text style={styles.offsetStyle}>
+              
+            </Text>
+          </View> :
+          <View style={styles.soloText}>
+            <Text style={styles.textStyle}>{this.props.mainText}</Text>
+            </View>}
             <View
               style={{
                 borderBottomColor: 'black',
@@ -29,9 +42,22 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
   },
   textViewStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   textStyle: {
     fontSize: 24,
+    alignItems: 'center',
+  },
+  iconStyle: {
+    paddingLeft:10,
+  },
+  offsetStyle: {
+    paddingRight:20
+  },
+  soloText: {
+    flexDirection: 'row',
+    justifyContent: 'center',
   }
 })

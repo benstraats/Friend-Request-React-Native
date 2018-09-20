84ea70f2c73be6308f1eaa6de74814c552207bf9
Ben Straatsma <benstraatsma@gmail.com>
2018-09-20 18:25:45 -0400

    Revert "better logout shit"
    This reverts commit acb2fb47468fe41c613437e35aa8082dce56067d.

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Alert, View, StyleSheet, Text, BackHandler} from 'react-native';
+import {Alert, View, StyleSheet, Text} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {STRINGS} from './utils/ProjectConstants'
 
@@ -20,7 +20,8 @@ export default class StatusBarOffset extends Component{
 
   logUserOut = () => {
     //TODO: wipe saved user info
-    BackHandler.exitApp();
+    this.props.navigation.goBack(null)
+    this.props.navigation.goBack(null)
   }
 
   render(){
